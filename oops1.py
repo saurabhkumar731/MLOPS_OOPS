6080892bd38518483df9ba752872366138011480
Saurabh <kumarsaurabh00435@gamil.com>
2025-10-09 13:28:56 +0530

    encapsulation ,getter setter and static method covered

--- oops1.py
+++ oops1.py
@@ -2,23 +2,31 @@
 class Employee:
     #special method/magic method/dunder method -constructor
     def __init__(self):
-        print("started executing attributes/data")
+        #print(id(self))
+        #print("started executing attributes/data")
         self.id=123
         self.salary=50000
         self.designation="SDE"
-        print("attributes/data have been initiated")
+        #print("attributes/data have been initiated")
         
-    def Travel(self,destination):
+    def Travel(self):
         print("This  travel method was called manually")
-        print(f"Employee in now travelling to {destination}")   
+        print(f"Employee in now travelling to Patna")   
         
 #create an obj/instance of the class        
-sam=Employee()  
+sam=Employee()
+# print(id(sam))
+
+# sam.name="Rajat"
+# print(sam.name)
+
+# shaktiman=Employee()
+# print(id(shaktiman))  
 
 #printing the attributes
 #print(sam.salary)   
 
 #calling a  method
-#sam.Travel("siwan")   
+#sam.Travel()   
 
-print(type(sam))
+#print(type(sam))
--- rough.py
+++ rough.py
@@ -13,5 +13,43 @@ my_int=242
 # print(a+b)
 
 from oops_proj import chatbook
-
 user1=chatbook()
+print(user1.id)
+
+
+
+# using static method directly from class rathe than obj
+# chatbook.set_id(10)
+
+# user2=chatbook()
+# print(user2.id)
+
+# user3=chatbook()
+# print(user3.id)
+
+# user2=chatbook()
+# print(user2.id)
+
+# user3=chatbook()
+# print(user3.id)
+
+
+
+#getter and setter
+# print(user1.get_name())
+# user1.set_name("ankit")
+# print(user1.get_name())
+
+
+# print(user1._chatbook__name)
+
+# #function vs method below
+# lst=[1,2,3]
+
+# #function
+# a1=len(lst)
+# print(a1)
+
+# #method
+# user1=chatbook()
+# user1.sendmsg()
